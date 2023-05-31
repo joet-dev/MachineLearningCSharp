eb55afbf76354553a60e8c1a7b62a9edd24902a7
joet-dev <joseph.thurlow7d0@gmail.com>
2023-05-31 19:26:40 +1000

    first commit

---
+++ myMLApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using MyMLApp;
+
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = "This restaurant sucked."
+};
+
+var result = SentimentModel.Predict(sampleData);
+
+var sentiment = result.PredictedLabel == 1 ? "positive" : "negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}"); 
